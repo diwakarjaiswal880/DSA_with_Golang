49005613df79585b87a44aceeb98b600faf73ec2
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2020-12-02 15:43:51 +0530

    Update linear_search_unsorted_list.go

--- linear_search_unsorted_list.go
+++ linear_search_unsorted_list.go
@@ -2,22 +2,21 @@ package main
 
 import (
 	"fmt"
-	
 )
 
 func main() {
-	a:=[]int{15,6,2,632,64,9,413,64}
-	v:=63
-	
-	fmt.Printf("In given array %v is present:%t",v,linearSearchUnsorted(a,v))
+	a := []int{15, 6, 2, 632, 64, 9, 413, 64}
+	v := 413
+
+	fmt.Printf("In given array %v is present:%t", v, linearSearchUnsorted(a, v))
 }
 
 func linearSearchUnsorted(data []int, value int) bool {
-size := len(data)
-for i := 0; i < size; i++ {
-if value == data[i] {
-return true
-}
-}
-return false
+	size := len(data)
+	for i := 0; i < size; i++ {
+		if value == data[i] {
+			return true
+		}
+	}
+	return false
 }
